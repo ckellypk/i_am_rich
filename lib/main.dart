b940b0a2b024478ee5a6d96742eeadd474af5201
ckellypk <ckelly@commure.com>
2022-01-13 13:45:04 -0500

    Creating starting branch

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+//This is the beginning of your flutter app. Notice there is a main mathod that runst the app by calling runApp
+//This is the entry point for all of you applications. 
+
+//s we are using Material widgets we are building a material app. The home of the app will just have a text of 
+//Hello World. 
+
+//The center widget centeres everything inside of it. 
+
+void main() => runApp(const MaterialApp(home: Center(child: Text('Hello World'))));
+
+
+
+
